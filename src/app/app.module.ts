393dbe00e5a7e9f138023388c71149a7627adc59
kavitha C L <devfourpc@outlook.com>
2019-09-20 17:20:57 +0530

    Validation for Password Mismatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { ErrorInterceptor } from './error-interceptor';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatDialogModule,  MatToolbarModule,  MatFormFieldModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -17,6 +19,8 @@ import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { ErrorComponent } from './error/error.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,16 +33,26 @@ import { ResetPasswordComponent } from './auth/reset-password/reset-password.com
     MenuComponent,
     DashboardComponent,
     ForgotPasswordComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatFormFieldModule
+
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   // providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent]
 })
 export class AppModule { }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { AuthService } from 'src/app/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -11,8 +11,8 @@ export class SignupComponent implements OnInit {
   singupForm: FormGroup;
   submitted = false;
   role = [2];
+  notsame = false;
   constructor(private formBuilder: FormBuilder, private authService: AuthService) {
-
   }
 
   ngOnInit() {
@@ -23,6 +23,19 @@ export class SignupComponent implements OnInit {
       confirmpassword: ['', Validators.required],
       adminCheck: ['']
     });
+    //   { validators: this.checkPasswords });
+  }
+
+  checkPasswords(pwd, cpwd) { // here we have the 'passwords' group
+    // const pass = group.get('password').value;
+    // const confirmPass = group.get('confirmpassword').value;
+    // console.log(confirmPass)
+    // if (pass !== '' && confirmPass !== '') {
+    //   return pass === confirmPass ? null : { notSame: true };
+    // }
+    // return false;
+    return pwd === cpwd ? null : { mismatch: true };
+
   }
 
 
@@ -39,8 +52,10 @@ export class SignupComponent implements OnInit {
     if (this.f.adminCheck.value) {
       this.role = [1];
     }
-   // console.log('Role for new User ', this.role);
-    this.authService.createUser(this.f.username.value, this.f.email.value, this.f.password.value, this.role);
+    const value = this.checkPasswords(this.f.password.value, this.f.confirmpassword.value);
+    if (!value) {
+      this.authService.createUser(this.f.username.value, this.f.email.value, this.f.password.value, this.role);
+    } else { this.notsame = true; }
   }
 
 }
